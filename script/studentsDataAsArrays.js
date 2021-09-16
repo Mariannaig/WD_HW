50c7642520cb508a11537929f9a613c84bbf9093
Mariannaig <86355017+Mariannaig@users.noreply.github.com>
2021-09-16 17:06:00 +0300

    fixed task

--- script/studentsDataAsArrays.js
+++ script/studentsDataAsArrays.js
@@ -4,39 +4,43 @@ let students = [
     ["female",22,"Maria","Petrova"],
     ["male",18,"Georgi","Georgiev"],
     ["male",21,"Nikolai","Nikolov"]
-];
-//////////////////// EXAMPLE ////////////////////
-// print in console the youngest student names: /
-/////////////////////////////////////////////////
-
-
-
-
-// inititalize minAge with first student age:
-let minAge = students[0][1];
-
-
-// variable to store youngest student data:
-let youngestStudent;
-
-
-let allAge=[];
-for (let i = 0; i < students.length; i++) {
+  ];
+  //////////////////// EXAMPLE ////////////////////
+  // print in console the youngest student names: /
+  /////////////////////////////////////////////////
+  
+  
+  
+  
+  // inititalize minAge with first student age:
+  let minAge = students[0][1];
+  
+  
+  // variable to store youngest student data:
+  let youngestStudent;
+  
+  
+  let allAge=[];
+  for (let i = 0; i < students.length; i++) {
     const ages = students[i][1];
-    allAge.push (ages);
-    // console.log(students[i][1]);
-    if(ages===18){
-        console.log(`Youngest student is ${students[i][2]} ${students[i][3]}`);
+  
+    if (ages<minAge) {
+      minAge=ages;
+      youngestStudent=students[i]
+          
     }
+   
      
     
-}
-// console.log(Math.min(...allAge)); тук установявам, че 18 е най-малката стойност и сега остава да го обрържем с името на студента
-
-
-
-
-
-
-
- 
+  }
+  
+  console.log(`The youngest student is ${youngestStudent[2]} ${youngestStudent[3]}. He is ${youngestStudent[1]} years old.`);
+  // console.log(Math.min(...allAge)); тук установявам, че 18 е най-малката стойност и сега остава да го обрържем с името на студента
+  
+  
+  
+  
+  
+  
+  
+  
